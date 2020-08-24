9ce842d21bb299df1bcc0de788d0d933e090f094
Sam Tymorek <samtymorek@gmail.com>
2020-08-24 10:17:36 -0500

    Adding new util file for some hardware stuff.

---
+++ src/util/hardware.py
@@ -0,0 +1,8 @@
+import mido
+
+def load_patches(path_to_syx_file):
+    # TODO: config
+    with mido.open_output('Scarlett 18i8 USB') as ioport:
+        for m in mido.read_syx_file(path_to_syx_file):
+            ioport.send(m)
+
